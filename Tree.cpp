2c36a1ea9d49b51ae18aa66a362d56cdca73c979
Aleksandra <75786246+Simalexa@users.noreply.github.com>
2021-06-29 12:48:27 +0300

    Add files via upload

--- Tree.cpp
+++ Tree.cpp
@@ -1,45 +1,22 @@
 #include <iostream>
 #include "TreeNode.h"
 
-void addNode(struct TreeNode *&node, int el)
-{
-	if (node == NULL)
-	{
-		node = new TreeNode;
-		node->element = el;
-		node->right = NULL;
-		node->left = NULL;
-	}
-	if (el < node->element)
-	{
-		addNode(node->left, el);
-	}
-	if (el > node->element)
-	{
-		addNode(node->right, el);
-	}
-}
-void printTree(struct TreeNode* node)
-{
-	if (node != NULL)
-	{
-		std::cout << node->element << " ";
-		printTree(node->left);
-		printTree(node->right);
-	}
-}
-
 int main()
 {
 	struct TreeNode* n;
 	n = NULL;
-    addNode(n, 8);
-	addNode(n, 3);
-	addNode(n, 6);
-	addNode(n, 1);
-	addNode(n, 10);
-	addNode(n, 14);
-	addNode(n, 7);
-    printTree(n);
+	tree t;
+    t.addNode(n, 8);
+	t.addNode(n, 3);
+	t.addNode(n, 6);
+	t.addNode(n, 1);
+	t.addNode(n, 10);
+	t.addNode(n, 14);
+	t.addNode(n, 5);
+    t.printTree(n);
+	t.dellNode(n, 5);
+	t.printTree(n);
+	//t.delTree(n);
+	//t.printTree(n);
     return 0;
 }
--- TreeNode.h
+++ TreeNode.h
@@ -1,11 +1,93 @@
 #ifndef TREENODE_H
 #define TREENODE_H
+#include <iostream>
 
 struct TreeNode
 {
 	struct TreeNode* left;
 	struct TreeNode* right;
-	int element; // Head Element
+	int element; // head of node
+	int number; // number of node
+};
+
+class tree
+{
+	private:
+		int size;
+	public:
+		tree()
+		{
+			int size = 0;
+		};
+		~tree()
+		{
+		};
+		void delTree(struct TreeNode*& node)
+		{
+			this->size = 0;
+			if (node != NULL)
+			{
+				delTree(node->left);
+				delTree(node->right);
+				node = NULL;
+			}
+		};
+		void printTree(struct TreeNode*& node)
+		{
+			if (this->size == 0)
+				std::cout << std::endl << "Tree is Empty" << std::endl;
+			else if (node != NULL)
+			{
+				std::cout << node->element << " ";
+				printTree(node->left);
+				printTree(node->right);
+			}
+		};
+		void addNode(struct TreeNode*& node, int el)
+		{
+			this->size++;
+			std::cout << this->size;
+			if (node == NULL)
+			{
+				node = new TreeNode;
+				node->element = el;
+				node->right = NULL;
+				node->left = NULL;
+				node->number = 1;
+			}
+			if (el < node->element)
+			{
+				node->number++;
+				addNode(node->left, el);
+			}
+			if (el > node->element)
+			{
+				node->number++;
+				addNode(node->right, el);
+			}
+		};
+		void dellNode(TreeNode*& node, int a)
+		{
+			//std::cout << this->size;
+			//	if (a <= size)
+			//	{
+			//		if (node->number == a && a < size)
+			//		{
+			//			node->element = node->right->element;
+			//			std::cout << "Click";
+			//			dellNode(node->right, a);
+			//		}
+			//		if (node->number == a && a == size)
+			//		{
+			//			node->element = NULL;
+			//			node->left = NULL;
+			//			node->right = NULL;
+			//			size--;
+			//		}
+			//	}
+			//	else
+			//		std::cout << "Error" << std::endl;
+		};
 };
 
 #endif
