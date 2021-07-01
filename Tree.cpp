6285885345eb9f53fb2935604020c00e264cc678
Aleksandra <75786246+Simalexa@users.noreply.github.com>
2021-07-01 22:51:15 +0300

    Add files via upload
    Add new treePaint which draw tree more beautifull.

--- Tree.cpp
+++ Tree.cpp
@@ -13,9 +13,11 @@ int main()
 	t.addNode(n, 10);
 	t.addNode(n, 14);
 	t.addNode(n, 5);
+	//t.findLevel(n, 1);
     t.printTree(n); // print Tree
-	int NodeNumber = t.findElement(n, 8); // find element in the tree
-	t.dellNode(n, NodeNumber); // delete node with element 8
-	t.printTree(n);
+	t.printVec(n);
+	//int NodeNumber = t.findElement(n, 8); // find element in the tree
+	//t.dellNode(n, NodeNumber); // delete node with element 8
+	//t.printTree(n);
     return 0;
 }
--- TreeNode.h
+++ TreeNode.h
@@ -1,13 +1,17 @@
 #ifndef TREENODE_H
 #define TREENODE_H
 #include <iostream>
+#include <math.h> 
+#include <string>
 
 struct TreeNode
 {
 	struct TreeNode* left;
 	struct TreeNode* right;
+	struct TreeNode* line;
 	int element; // head of node
 	int number; // number of node
+	int level; //level of node
 };
 
 class tree
@@ -15,6 +19,11 @@ class tree
 	private:
 		int size; // size of tree
 		int nodeToDel; // index of Null element in tree,  which used in deleting node function 
+		int maxLevel; // maximum level
+		// for creating Beutifull tree
+		//std::string vec;
+		int vec[10];
+		int count;
 		void moveNodeUp(TreeNode*& node) // finction to move all elements up when deleteNode is active
 		{
 			if (node->right != NULL)
@@ -49,10 +58,40 @@ class tree
 					rightIndex(node->right);
 				}
 		}
+		void elementInLevel(struct TreeNode*& node, int lev)
+		{
+			if (node != NULL && node->number <= size)
+			{
+				if (node->level == lev)
+				{
+					vec[count] = node->element;
+					count++;
+				}
+				elementInLevel(node->left, lev);
+				elementInLevel(node->right, lev);
+			}
+		};
+		void findLevel(struct TreeNode*& node, int inL)
+		{
+			if (node != NULL)
+			{
+				node->level = inL;
+				if (node->left != NULL)
+					node->left->level = inL + 1;
+				if (node->right != NULL)
+					node->right->level = inL + 1;
+				if (node->level > maxLevel)
+					maxLevel = node->level;
+				findLevel(node->right, inL + 1);
+				findLevel(node->left, inL + 1);
+			}
+		};
 	public:
 		tree()
 		{
 			size = 0;
+			maxLevel = 0;
+			count = 0;
 		};
 		~tree()
 		{
@@ -76,13 +115,32 @@ class tree
 				if (node->number == 1)
 				{
 					std::cout << "Tree in prefics form: " << std::endl;
+					findLevel(node, 1);
 				}
-				std::cout << node->element << " (" << node->number << ") ";
+				std::cout << node->element << " (" << node->level << ") ";
 				printTree(node->left);
 				printTree(node->right);
 			}
 			if(node != NULL && node->number == this-> size - 1)
 				std::cout << std::endl;
+		};
+		void printVec(struct TreeNode*& node)
+		{
+			for (int i = 1; i <= maxLevel; i++)
+			{
+				elementInLevel(node, i);
+				for (int k = 0; k < count; k++)
+				{
+					for (int j = 0; j < pow(2.0, maxLevel - i) - 1; j++)
+						std::cout << " ";
+					std::cout << vec[k];
+					for (int j = 0; j < pow(2.0, maxLevel - i) - 1; j++)
+						std::cout << " ";
+					vec[k] = NULL;
+				}
+				count = 0;
+				std::cout << std::endl;
+			}
 		};
 		void addNode(struct TreeNode*& node, int el) // add new node
 		{
